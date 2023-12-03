cddc94ce993d6119309d9a7732a9e2f38268ac74
martin220485 <martin220485@gmail.com>
2023-12-03 21:35:12 +0100

    home.tsx

--- pages/api/home/home.tsx
+++ pages/api/home/home.tsx
@@ -303,7 +303,7 @@ const Home = ({ defaultModelId }: Props) => {
       }}
     >
       <Head>
-        <title>Chatbot Ollama</title>
+        <title>IA Eurospital</title>
         <meta name="description" content="ChatGPT but local." />
         <meta
           name="viewport"
